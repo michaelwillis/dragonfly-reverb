3384f01625880f2384c47905b4c4ce42bf3b356d
Rob van den Berg <rghvdberg@gmail.com>
2018-03-09 08:59:33 +0100

    Remove unneeded variables

--- plugins/dragonfly-reverb/DragonflyReverbPlugin.hpp
+++ plugins/dragonfly-reverb/DragonflyReverbPlugin.hpp
@@ -112,12 +112,7 @@ private:
     float late_in_buffer[2][BUFFER_SIZE];
     float late_out_buffer[2][BUFFER_SIZE];
 
-    // needed for initializing the buttons
-
-    float play = 1.0f; // default to 'on'
-    float programs = 0.0f;
-    float about = 0.0f;
-
+ 
     DISTRHO_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(DragonflyReverbPlugin)
 };
 
