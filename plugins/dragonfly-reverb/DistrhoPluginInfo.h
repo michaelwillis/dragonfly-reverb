a5519a56a39cdac5d391706c67fab001fd20838a
Michael Willis <willismichael@gmail.com>
2018-10-17 12:23:13 -0600

    Update license documentation

--- plugins/dragonfly-reverb/DistrhoPluginInfo.h
+++ plugins/dragonfly-reverb/DistrhoPluginInfo.h
@@ -1,6 +1,6 @@
 /*
  * Dragonfly Reverb, a hall-style reverb plugin
- * Copyright (c) 2018 Michael Willis
+ * Copyright (c) 2018 Michael Willis, Rob van den Berg
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
