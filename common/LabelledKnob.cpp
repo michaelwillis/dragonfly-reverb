c4cb2f308faec2a85b35f96c702aa08ca9a24eb0
Michael Willis <willismichael@gmail.com>
2019-11-20 19:20:22 -0700

    Refactor LabelledKnob

--- common/LabelledKnob.cpp
+++ common/LabelledKnob.cpp
@@ -1,6 +1,5 @@
 /*
- * Dragonfly Reverb, a hall-style reverb plugin
- * Copyright (c) 2018 Michael Willis, Rob van den Berg
+ * Dragonfly Reverb, copyright (c) 2019 Michael Willis, Rob van den Berg
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
