6fbbad10a7e8c1ba8935613050320012cf3db923
Shane Dunne <shane@getdunne.com>
2020-04-08 12:51:17 -0400

    JUCE plugins: add GPL3/copyright header comments to all source files

--- juce-plugins/DragonflyPlate/Source/DragonflyPlateEditor.h
+++ juce-plugins/DragonflyPlate/Source/DragonflyPlateEditor.h
@@ -1,3 +1,20 @@
+/*
+ * Dragonfly Reverb
+ * copyright (c) 2019-2020 Michael Willis, Rob van den Berg, Shane Dunne
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * For a full copy of the GNU General Public License see the LICENSE file.
+ */
+
 #pragma once
 #include "JuceHeader.h"
 #include "DragonflyPlateProcessor.h"
