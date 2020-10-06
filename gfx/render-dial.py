c44daf1c0289e756517afe23a2dbff039ad898f7
Michael Willis <willismichael@gmail.com>
2020-10-05 21:14:18 -0600

    WIP graphics for large dial

--- gfx/render-dial.py
+++ gfx/render-dial.py
@@ -1,15 +1,16 @@
 from PIL import Image
 
-b = Image.open("knob-background.png")
-l = Image.open("knob-line.png")
-bs = Image.new("RGBA", (32, 3232))
-ls = Image.new("RGBA", (32, 3232))
+b = Image.open("knob-background-52.png")
+l = Image.open("knob-line-52.png")
+bs = Image.new("RGBA", (52, 5252))
+ls = Image.new("RGBA", (52, 5252))
 
 for row in range(101):
-    offset = (0, row * 32)
+    offset = (0, row * 52)
     bs.paste(b, offset)
     ls.paste(l.copy().rotate(135.0 - (float(row) * 0.01 * 270)), offset)
 
-bs.save("knob-backgrounds.png")
-ls.save("knob-lines.png")
+bs.save("knob-backgrounds-52.png")
+ls.save("knob-lines-52.png")
+
 
